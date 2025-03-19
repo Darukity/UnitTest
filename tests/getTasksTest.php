d3a87a60cb9b18c62a969d0a0bd318e3bcf0e685
Darukity <samchgaming@gmail.com>
2025-03-19 10:38:16 +0100

    Renamed get Task test because i wrote Tests instead of Test at the end of the file

--- tests/getTasksTest.php
+++ tests/getTasksTest.php
@@ -10,12 +10,13 @@ class GetTasksTest extends TestCase
 {
     public function testGetTasks()
     {
+        print("Test GetTasks\n");
         $taskManager = new TaskManager();
         $taskManager->addTask('Task 1');
         $taskManager->addTask('Task 2');
 
         $tasks = $taskManager->getTasks();
-        
+
         $this->assertCount(2, $tasks);
         $this->assertEquals('Task 1', $tasks[0]);
         $this->assertEquals('Task 2', $tasks[1]);
